1329538767642913498262b7e82fe18eebe176d8
Ronaldo Trias <ronaldotrias@MacBook-Pro.local>
2022-11-08 13:05:31 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+
+const App = () => {
+
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exersises1 = 10
+    const part2 = 'Using props to pass data'
+    const exersises2 = 7
+    const part3 = 'State of a component'
+    const exersises3 = 14
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+      <>
+        <h1>{course}</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {part1} {exersises1}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      
+        <p>
+          {part2} {exersises2}
+        </p>
+
+      
+        <p>
+          {part3} {exersises3}
+        </p>
+
+      <p>Number of exersises: {exersises1 + exersises2 + exersises3}</p>
+      
+      </>
+  )
 }
+
 
 export default App;
