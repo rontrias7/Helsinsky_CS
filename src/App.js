4b9bad56dbcccad8a91dbc1216b37bc6c0baacf5
rontrias7 <rontrias7@gmail.com>
2022-11-08 13:47:22 -0300

    Refactoring components and passing data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { isContentEditable } from "@testing-library/user-event/dist/utils"
 
 
 
@@ -16,27 +17,65 @@ const App = () => {
   return (
 
       <>
-        <h1>{course}</h1>
+        <Header course={course} />
+        <Content part={part1} exersises={exersises1} />
+        <Content part={part2} exersises={exersises2} />
+        <Content part={part3} exersises={exersises3} />
+        <Total total={exersises1 + exersises2 + exersises3}/>
 
-        <p>
-          {part1} {exersises1}
-        </p>
 
-      
-        <p>
-          {part2} {exersises2}
-        </p>
 
       
-        <p>
-          {part3} {exersises3}
-        </p>
-
-      <p>Number of exersises: {exersises1 + exersises2 + exersises3}</p>
       
       </>
   )
 }
+
+
+const Header = (props) =>{
+
+return (
+
+  <>
+    <h1>{props.course}</h1>
+  </>
+
+
+)
+
+}
+
+
+const Content = (props) =>{
+
+  return (
+  <>
+   
+    <p>
+      {props.part} {props.exersises}
+    </p>
+  </>
+
+  );
+  
+  }
+
+
+
+
+
+  const Total = (props) =>{
+
+    return (
+    <>
+    
+    <p>Number of exersises: {props.total}</p>
+    
+    </>
+    
+    )
+    
+    }
 
 
 export default App;
